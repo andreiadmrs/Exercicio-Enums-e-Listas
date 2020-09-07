4c4271edee5d098659d7e7f070bad94949e463c8
Andreia de Moraes <andreiadmrs@gmail.com>
2020-09-07 13:59:18 -0300

    Exercício do curso de C#. Enums e Listas.

---
+++ Enum1/Entities/Worker.cs
@@ -0,0 +1,49 @@
+﻿using Enum1.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Enum1.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;         
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract hourContract in Contracts)
+            {
+                if (hourContract.Date.Year == year && hourContract.Date.Month == month)
+                {
+                    sum += hourContract.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Enum1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using Enum1.Entities;
+using Enum1.Entities.Enums;
+
+namespace Enum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter department's name");
+            string depName = Console.ReadLine();
+            Console.WriteLine("Enter worker data:");
+            Console.WriteLine("Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Level: (Junior/MidLevel/Senior):");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.WriteLine("Base Salary:");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(depName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.WriteLine("How many contracts to this worker?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data");
+                Console.WriteLine("Date (DD/MM/YYYY):");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Value per hour:");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Duration (Hours):");
+                int hours = int.Parse(Console.ReadLine());
+
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter month and year to calculate income (MM/YYYY):");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));           
+
+            Console.WriteLine("Name "+ worker.Name);
+            Console.WriteLine("Department "+ worker.Department.Name);
+            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month).ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+
+    }
+}
